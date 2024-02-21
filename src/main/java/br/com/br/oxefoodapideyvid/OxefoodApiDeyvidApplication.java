3dcbcd533dfc352c30f2cb05237db6afde5bada1
aluno <aluno@ifpe>
2024-02-21 18:18:49 -0300

    https://github.com/Deyvid00/oxefood-api-deyvid.git

---
+++ src/main/java/br/com/br/oxefoodapideyvid/OxefoodApiDeyvidApplication.java
@@ -0,0 +1,13 @@
+package br.com.br.oxefoodapideyvid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxefoodApiDeyvidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxefoodApiDeyvidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/br/oxefoodapideyvid/OxefoodApiDeyvidApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.br.oxefoodapideyvid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OxefoodApiDeyvidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
